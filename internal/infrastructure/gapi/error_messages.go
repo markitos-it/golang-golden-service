e33444dfda87c4422073b5377eb38b0d4c17db30
Marco Antonio - markitos <markitos.es.info@gmail.com>
2026-07-13 17:41:39 +0200

    refactor: format error messages for consistency

--- internal/infrastructure/gapi/error_messages.go
+++ internal/infrastructure/gapi/error_messages.go
@@ -1,9 +1,9 @@
 package gapi
 
 const (
-	msgInvalidRequest     = "invalid request"
-	msgResourceNotFound   = "resource not found"
-	msgInternalServerErr  = "internal server error"
-	msgInvalidPageNumber  = "invalid page number"
-	msgInvalidPageSize    = "invalid page size"
+	msgInvalidRequest    = "invalid request"
+	msgResourceNotFound  = "resource not found"
+	msgInternalServerErr = "internal server error"
+	msgInvalidPageNumber = "invalid page number"
+	msgInvalidPageSize   = "invalid page size"
 )
