046a58ede57585f9ec5c1c96e30a7fbc40610ea8
Marco Antonio - markitos <markitos.es.info@gmail.com>
2026-07-13 17:44:55 +0200

    refactor: remove unused error messages for page validation

--- internal/infrastructure/gapi/error_messages.go
+++ internal/infrastructure/gapi/error_messages.go
@@ -4,6 +4,4 @@ const (
 	msgInvalidRequest    = "invalid request"
 	msgResourceNotFound  = "resource not found"
 	msgInternalServerErr = "internal server error"
-	msgInvalidPageNumber = "invalid page number"
-	msgInvalidPageSize   = "invalid page size"
 )
